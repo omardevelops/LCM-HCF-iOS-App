d705a3e50bdf8224e15ea622728c92e9d910317d
Omar Ahmad <superomar121@gmail.com>
2020-10-03 02:29:36 -0700

    Initial commit

---
+++ LCM-HCF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LCM-HCF
+//
+//  Created by Omar Ahmed on 10/3/20.
+//  Copyright © 2020 Omar Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LCM-HCFTests/LCM_HCFTests.swift
@@ -0,0 +1,34 @@
+//
+//  LCM_HCFTests.swift
+//  LCM-HCFTests
+//
+//  Created by Omar Ahmed on 10/3/20.
+//  Copyright © 2020 Omar Ahmad. All rights reserved.
+//
+
+import XCTest
+@testable import LCM_HCF
+
+class LCM_HCFTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
